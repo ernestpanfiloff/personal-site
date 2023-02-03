9aa5a42254eb4770228082a288844a3cbd081f07
ernest <ernestas.du@gmail.com>
2023-02-03 23:53:20 +0000

    reponsive logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ export default function Header() {
 
 
             <h1 className="Header-Title">Ernest Panfiloff</h1>
+            <h1 className="Header-Title-Mobile">EP</h1>
             <nav className="Header-Links">
                 <a href="#about">About</a>
                 <a href="#projects">Projects</a>
