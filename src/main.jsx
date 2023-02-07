95f8f5f445d6522c7828103b7d73f30a8f8fb015
ernest <ernestas.du@gmail.com>
2023-02-07 06:05:09 +0000

    cleanup

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import './main.css'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Projects from './components/Projects'
+import projects from './data/projects'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <div className="container">
+        <Header />
+        <Hero />
+        <Projects projects={projects} />
+      </div>
   </React.StrictMode>
 )
