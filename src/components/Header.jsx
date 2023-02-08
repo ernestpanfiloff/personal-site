d485f249434646fc5b3b5198aecd1fa2c9e57ac8
ernest <ernestas.du@gmail.com>
2023-02-08 21:22:34 +0000

    removed header links for now

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,9 +8,9 @@ export default function Header() {
             <h1 className="Header-Title"><a href="/" alt="Ernest Panfiloff Home Page">Ernest Panfiloff</a></h1>
             <h1 className="Header-Title-Mobile">EP</h1>
             <nav className="Header-Links">
-                <a href="#about">About</a>
+                {/* <a href="#about">About</a>
                 <a href="#projects">Projects</a>
-                <a href="#contact">Contact</a>
+                <a href="#contact">Contact</a> */}
             </nav>
 
 
