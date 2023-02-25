a3102f04421b231a28e2b94a24d8ab736cb69f01
ernest <ernestas.du@gmail.com>
2023-02-25 10:07:55 +0000

    added blog link to the navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,9 +8,7 @@ export default function Header() {
             <h1 className="Header-Title"><a href="/" alt="Ernest Panfiloff Home Page">Ernestas Panfilovas</a></h1>
             <h1 className="Header-Title-Mobile">EP</h1>
             <nav className="Header-Links">
-                {/* <a href="#about">About</a>
-                <a href="#projects">Projects</a>
-                <a href="#contact">Contact</a> */}
+                <a href="https://medium.com/@ernestpanfiloff" target="_blank">Blog</a>
             </nav>
 
 
